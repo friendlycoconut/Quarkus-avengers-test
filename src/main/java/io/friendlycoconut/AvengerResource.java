8f0013733d7bfca65f087a53a31ca86a065c9bdf
friendlycoconut <kosten.il2000@gmail.com>
2024-01-05 14:00:15 +0100

    added openshift conn

--- src/main/java/io/friendlycoconut/AvengerResource.java
+++ src/main/java/io/friendlycoconut/AvengerResource.java
@@ -1,25 +1,7 @@
 package io.friendlycoconut;
 
-import jakarta.transaction.Transactional;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 
-import java.util.List;
-@Path("/avenger")
-public class AvengerResource {
-
-    @GET
-    public List<?> getAll() {
-        return Avenger.listAll();
-    }
-
-    @POST
-    @Transactional
-    public Avenger create(Avenger avenger){
-        avenger.persist();
-        return avenger;
-    }
+public interface AvengerResource extends PanacheEntityResource<Avenger, Long> {
+    
 }
